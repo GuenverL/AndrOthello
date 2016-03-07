2920fecd2dbc3b1ae7f9bb43e0e7b04210b48848
LetayB <sun0post@gmail.com>
2016-03-07 10:38:24 +0100

    Gros boost Interface + architecture nécessaire motor

--- app/src/main/java/Androthello/Model/Motor.java
+++ app/src/main/java/Androthello/Model/Motor.java
@@ -5,7 +5,19 @@ package androthello.model;
  */
 public class Motor {
 
-    public static void MakeaMove(int col, int row){
+    public static void MakeMove(int col, int row){
 
+    }
+
+    public static int getPlayerCount(int playerID){
+        return 2;
+    }
+
+    public static int getActivePlayerID(){
+        return 1;
+    }
+
+    public static CellState GetCellState(int col,int row){
+        return CellStateEmpty.getInstance();
     }
 }
